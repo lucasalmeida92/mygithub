6bf5b4b415883bd590c062a0f9345f8f7aa9e89d
Lucas Almeida MAC <lucasalmeida70@gmail.com>
2018-06-01 20:54:02 -0300

    Repositories list layout

--- src/containers/RepositoriesPage/Repository/index.js
+++ src/containers/RepositoriesPage/Repository/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FA from 'react-fontawesome';
 import './index.scss';
 
 class Repository extends Component {
@@ -18,7 +19,11 @@ class Repository extends Component {
 
     return (
       <a className="Repository" href="#" onClick={(e) => this.handleOnClick(e, repository.name)} title={repository.name}>
-        <h3 className="Repository__name">{repository.name}</h3>
+        <h3 className="Repository__name">{ repository.name }</h3>
+        <p className="Repository__description">{ repository.description }</p>
+        <span className="Repository__stars">
+          <FA name="star" /> {repository.stargazers_count} stars
+        </span>
       </a>
     );
   }
